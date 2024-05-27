fce7ac12eb73128d57461399aadb8dd3832f1e19
Justine <justine-huang@tm.jfe-eng.co.jp>
2024-05-27 11:21:36 +0800

    Feat: Starfield

--- src/components/home/Landing.js
+++ src/components/home/Landing.js
@@ -1,11 +1,12 @@
 import * as React from "react"
 import styled from "styled-components";
 import bg from "../../images/bg_space.jpg"
+import PaperCanvas from "../features/Page_1";
 
 const Landing = ({scrollRef}) => {
 	return(
 		<LandingDiv className="container-fluid p-0" ref={scrollRef}>
-			
+			<PaperCanvas></PaperCanvas>
 		</LandingDiv>
 	)
 }
@@ -13,7 +14,9 @@ const Landing = ({scrollRef}) => {
 export default Landing;
 
 const LandingDiv = styled.div`
-	background-image: url(${bg});
+	height: 100vh;
+
+	/* background-image: url(${bg});
 	// background-color: #290029;
 	height: 100vh;
 
@@ -21,10 +24,10 @@ const LandingDiv = styled.div`
 		position: absolute;
 		width: inherit;
 		height: inherit;
-		background: rgba(69, 42, 94, 0.5);
+		//background: rgba(69, 42, 94, 0.5);
 	}
 
 	.landing-col {
 		margin-top: 100px;
-	}
+	} */
 `
